29139b47a527446c1558d7fab40f58b61e4c0380
Fullstack23 <full54078@gmail.com>
2024-04-29 19:36:44 -0300

    feat: criar_uma_lista_de_usuarios.js

---
+++ Aula_6/criar_uma_lista_de_usuarios.js
@@ -0,0 +1,28 @@
+// 1 - Necessito criar um lista de usuários com os nomes
+// Sofia
+// Lucas
+// Isabella
+// Mateus
+// Alice
+// 
+// Essa lista deve ser armazenada em uma constante.
+// Quero que na tela seja impresso na saída os nomes, Sofia, Isabella e Alice.
+// Dica: Será necessário utilizar um recurso chamado ARRAY
+
+//Exemplo 1
+// const listas = ["Sofia", "Lucas", "Isabella", "Mateus"];
+// listas[4] = "Alice";
+// listas.forEach((item, index) => {
+//     console.log(`${index}: ${item}`);
+//     listas.reverse();
+// });
+
+//Exemplo 2
+// const listas = ['Sofia', 'Lucas', 'Isabella', 'Mateus', 'Alice'];
+// console.log(listas.join());
+// console.log(listas.join(''));
+// console.log(listas.join('-'));
+
+
+const listas = ["Sofia", "Lucas", "Isabella", "Mateus","Alice"];
+console.log(listas.pop());
